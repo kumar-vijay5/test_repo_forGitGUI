4adef87d26a48c57e99a3061b7385fc4097bf896
kumar-vijay5 <kumars.vijay05@gmail.com>
2022-09-17 16:16:13 +0530

    Update HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -1,6 +1,5 @@
 public class HellowWorld {
   public void getScope() {
-    System.out.println("This is a hellowWorld java class method");
-    System.out.println("This is a updated");
+    System.out.println("This is a hellowWorld java class method");    
   }
 }
